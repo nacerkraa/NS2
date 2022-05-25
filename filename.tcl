76532509b2c2be720db4042792c933766f7b75e8
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-05-25 20:49:45 +0000

    print multiple vlaues

--- filename.tcl
+++ filename.tcl
@@ -1,2 +1,6 @@
 set a 14 ; # To set a value to a variable
+set b Hello ; # To set a value to a variable
+set c 2.3 ; # To set a value to a variable
 puts $a  ; # To print the value
+puts $b  ; # To print the value
+puts $c  ; # To print the value
