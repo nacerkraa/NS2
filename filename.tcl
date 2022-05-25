c5ea827a5e008af4fd08e8b8284de44d1502321f
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-05-25 20:51:04 +0000

    update

--- filename.tcl
+++ filename.tcl
@@ -1,6 +1,6 @@
 set a 14 ; # To set a value to a variable
-set b Hello ; # To set a value to a variable
-set c 2.3 ; # To set a value to a variable
+set b Hello 
+set c 2.3 
 puts $a  ; # To print the value
 puts $b 
 puts $c  
