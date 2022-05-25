13ae5ef8ae53bf004433bea5da50b6bb6eaa923d
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-05-25 21:09:30 +0000

    print two line

--- filename.tcl
+++ filename.tcl
@@ -2,5 +2,5 @@ set a 14 ; # To set a value to a variable
 set x $a  ; # affectation
 set r [expr $a - 10]
 
-puts $x  ; # To print the value
-puts $r  ; # To print the value
+puts "this a line"
+puts "this is a new line" 
