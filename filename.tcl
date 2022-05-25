67692180bbf1d5404e06a4905f119d9f933138e9
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-05-25 21:40:52 +0000

    condition if else

--- filename.tcl
+++ filename.tcl
@@ -1,5 +1,8 @@
-set a 14 ; # To set a value to a variable
+set a 5 ; # To set a value to a variable
 
 if {$a > 10} {
     puts "a is greater then 10"
+} else {
+    puts "a is less then 10"
+    
 }
