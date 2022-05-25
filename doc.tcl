a4a2b18ab6abf031b08316860b9ead7eada36a0c
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-05-25 22:15:54 +0000

    update

--- doc.tcl
+++ doc.tcl
@@ -6,6 +6,15 @@ puts $a  ; # To print the value
 
 puts -nonewline "this a line"
 
+# if statment
 if {$a > 10} {
     puts "a is greater then 10"
+}
+
+# if else
+if [expr $a > 10] {
+    puts "a is greater then 10"
+} else {
+    puts "a is less then 10"
+    
 }
