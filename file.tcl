5de0ac892902f4473f46efaccf282ad6a4b92776
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-05-25 20:32:14 +0000

    update

--- file.tcl
+++ file.tcl
@@ -11,7 +11,6 @@ set val(stop)   10.0;               # time of simulation end
 #===================================
 #Create a ns simulator
 set ns [new Simulator]
-
 #Open the NS trace file
 set tracefile [open out.tr w]
 $ns trace-all $tracefile
