662be3a8b93445e4431804d62abd92b7ea1a41c5
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-05-25 22:24:20 +0000

    simple exemple of simulation

--- filename.tcl
+++ filename.tcl
@@ -1,6 +1,21 @@
-set a 10 ; # To set a value to a variable
-
-while {$a > 1} {
-    puts "Hello world!"
-    set a [expr $a - 1]
+#Create a simulator object
+set ns [new Simulator]
+#Open trace files
+set f [open out.tr w]
+$ns trace-all $f
+#Define a 'finish' procedure
+proc finish {} {
+global ns
+$ns flush-trace
+close $f
+exit 0
 }
+#Create two nodes
+set n0 [$ns node]
+set n1 [$ns node]
+#Create a duplex link between the nodes
+$ns duplex-link $n0 $n1 1Mb 10ms DropTail
+#Call the finish procedure after 5 seconds of simulation time
+$ns at 5.0 "finish"
+#Run the simulation
+$ns run
