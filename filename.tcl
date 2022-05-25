6aabf1c41f02e3f41a76998598c5be4519b89397
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-05-25 23:02:16 +0000

    delete out.tr

--- filename.tcl
+++ filename.tcl
@@ -2,3 +2,7 @@
 ### simulaton app form the scratch #####
 ########################################
 
+# create the simulater
+set ns [new Simulater]
+
+# create two files to save the simulation in it
