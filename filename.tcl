2ed461b431642869e82dc849999a701d9fc02036
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-05-25 22:40:15 +0000

    update

--- filename.tcl
+++ filename.tcl
@@ -1,8 +1,10 @@
 #Create a simulator object
 set ns [new Simulator]
+
 #Open trace files
 set f [open out.tr w]
 $ns trace-all $f
+
 #Define a 'finish' procedure
 proc finish {} {
 global ns
@@ -10,12 +12,32 @@ $ns flush-trace
 close $f
 exit 0
 }
+
 #Create two nodes
 set n0 [$ns node]
 set n1 [$ns node]
+
 #Create a duplex link between the nodes
 $ns duplex-link $n0 $n1 1Mb 10ms DropTail
-#Call the finish procedure after 5 seconds of simulation time
+
+#Create a UDP agent and attach it to node n0
+set udp0 [new Agent/UDP]
+$ns attach-agent $n0 $udp0
+
+# Create a CBR traffic source and attach it to udp0
+set cbr0 [new Application/Traffic/CBR]
+$cbr0 set packetSize_ 500
+$cbr0 set interval_ 0.005
+$cbr0 attach-agent $udp0
+
+#Create a Null agent (a traffic sink) and attach it to node n1
+set null0 [new Agent/Null]
+$ns attach-agent $n1 $null0
+
+#Connect the traffic source with the traffic sink
+$ns connect $udp0 $null0
+#Schedule events for the CBR agent
+$ns at 0.5 "$cbr0 start"
+$ns at 4.5 "$cbr0 stop"
 $ns at 5.0 "finish"
-#Run the simulation
 $ns run
