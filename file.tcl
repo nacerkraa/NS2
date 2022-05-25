3e9bcfb99570f491f4e58d1249b6e9a7ce7ffd8f
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-05-25 20:26:41 +0000

    update

--- file.tcl
+++ file.tcl
@@ -10,6 +10,7 @@ set val(stop)   10.0                         ;# time of simulation end
 #        Initialization        
 #===================================
 #Create a ns simulator
+
 set ns [new Simulator]
 
 #Open the NS trace file
